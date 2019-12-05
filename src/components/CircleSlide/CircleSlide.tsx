08712643d34705effc355686d07506c395383f90
Rudy <rudyolsson@gmail.com>
2019-12-05 09:53:09 +0100

    add slide styles

--- src/components/CircleSlide/CircleSlide.tsx
+++ src/components/CircleSlide/CircleSlide.tsx
@@ -5,13 +5,11 @@ export default function CircleSlide({ images } : any) {
 
     return (
         <div className="tile" >
-            <div className="mask">
                 { images.map((image: any, i: any) => {
                     return (
                         <img src={require(`assets/images/${image.src}`)} alt={image.name} key={i}/>
                     )
                 })} 
-            </div>
         </div>
     )
 }
