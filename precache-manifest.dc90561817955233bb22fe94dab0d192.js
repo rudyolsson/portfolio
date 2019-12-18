aeabdf923fd92f1bd597e7708549e535328f5490
Rudy <rudyolsson@gmail.com>
2019-12-18 15:22:55 +0000

    Updates

---
+++ precache-manifest.dc90561817955233bb22fe94dab0d192.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd5fb3b2f357776bf23b94ea8c12ff98",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "06e89e53a62c5ac52ff2",
+    "url": "/portfolio/static/css/main.84baceb3.chunk.css"
+  },
+  {
+    "revision": "b0a9715a0c6e7e5c09a6",
+    "url": "/portfolio/static/js/2.b3f9c300.chunk.js"
+  },
+  {
+    "revision": "06e89e53a62c5ac52ff2",
+    "url": "/portfolio/static/js/main.b0ed8fa3.chunk.js"
+  },
+  {
+    "revision": "410e62daf606044135f6",
+    "url": "/portfolio/static/js/runtime-main.d3a7ab69.js"
+  },
+  {
+    "revision": "e6c5aab768bd914f597f0dc3d947b153",
+    "url": "/portfolio/static/media/artys-screens.e6c5aab7.webp"
+  },
+  {
+    "revision": "943a28ba8d452b2d60bf9ee01db18ce3",
+    "url": "/portfolio/static/media/flexo-three-screens.943a28ba.webp"
+  },
+  {
+    "revision": "b1e991859bde533485701a393f15492f",
+    "url": "/portfolio/static/media/ro.b1e99185.png"
+  },
+  {
+    "revision": "97e496b38443aa31a44e5a0e2bb4ffd8",
+    "url": "/portfolio/static/media/token-admin-ui.97e496b3.webp"
+  },
+  {
+    "revision": "e79f4989bc5e14200d31e4c4e1da07ba",
+    "url": "/portfolio/static/media/umatch-three-screens.e79f4989.webp"
+  }
+]);
